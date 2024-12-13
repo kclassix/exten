d26efd3b9b25e553a272c61c7871377d86ab0ab3
kclassix <63685521+kclassix@users.noreply.github.com>
2024-12-14 00:56:41 +0100

    Update extension5.mjs

--- extension5.mjs
+++ extension5.mjs
@@ -1,21 +1,16 @@
 import puppeteer from 'puppeteer';
 import * as chromeLauncher from 'chrome-launcher';
 import fs from 'fs';
-import { app, dialog } from 'electron';
-import decompress from "decompress";
-// import { spawn } from 'child_process';
-
+import { app } from 'electron';
 
 let userDocument = app.getPath('documents')  + '/Documents';
 
 // console.log(userDocument)
 
 let countryRandom = '#app > div.dashboard-layout.grid.h-full.top-0 > div.row-2.relative.overflow-hidden.col-start-2 > div > div.mx-auto.flex.w-full.max-w-\\[var\\(--dash-max-content-width\\)\\].shrink-0.grow.flex-col.bg-\\[inherit\\].px-24.\\@container.sm\\:px-32 > div > div.mt-24.flex.flex-col.gap-20 > div:nth-child(5) > div.flex.flex-col.gap-20.transition-opacity > div > section:nth-child(1) > div > div.\\@container > div.mt-24.grid.grid-cols-1.items-center.gap-x-20.\\@md\\:grid-cols-2.\\@3xl\\:grid-cols-4 > div:nth-child(1) > label > div.relative.w-full > div > div > div > span';
-let firstWaitSelector = '#app > div.dashboard-layout.grid.h-full.top-0 > div.row-2.relative.overflow-hidden.col-start-2 > div > div.mx-auto.flex.w-full.max-w-\\[var\\(--dash-max-content-width\\)\\].shrink-0.grow.flex-col.bg-\\[inherit\\].px-24.\\@container.sm\\:px-32 > div > div.mt-24.flex.flex-col.gap-20 > div:nth-child(4)';
 let skipISPStaticSelector = 'div.\\@container > div.tp-body-s.flex.flex-col.gap-20.border-b.border-neutral-100.pb-24.sm\\:flex-row.sm\\:items-center > div > label:nth-child(2) > button';
 let highEndPoolSelector = '#app > div.dashboard-layout.grid.h-full.top-0 > div.row-2.relative.overflow-hidden.col-start-2 > div > div.mx-auto.flex.w-full.max-w-\\[var\\(--dash-max-content-width\\)\\].shrink-0.grow.flex-col.bg-\\[inherit\\].px-24.\\@container.sm\\:px-32 > div > div.mt-24.flex.flex-col.gap-20 > div:nth-child(5) > div.flex.flex-col.gap-20.transition-opacity > div > section:nth-child(1) > div > div.\\@container > div.tp-body-s.flex.flex-col.gap-20.border-b.border-neutral-100.pb-24.sm\\:flex-row.sm\\:items-center > div > label:nth-child(1) > button';
 let scrollToSelector = 'div.\\@container > div.mt-24.grid.grid-cols-1.items-center.gap-x-20.\\@md\\:grid-cols-2.\\@3xl\\:grid-cols-4 > div:nth-child(1) > label > div.relative.w-full > div';
-let ifCountrySelectorOccupied = '#app > div.dashboard-layout.grid.h-full.top-0 > div.row-2.relative.overflow-hidden.col-start-2 > div > div.mx-auto.flex.w-full.max-w-\\[var\\(--dash-max-content-width\\)\\].shrink-0.grow.flex-col.bg-\\[inherit\\].px-24.\\@container.sm\\:px-32 > div > div.mt-24.flex.flex-col.gap-20 > div:nth-child(5) > div.flex.flex-col.gap-20.transition-opacity > div > section:nth-child(1) > div > div.\\@container > div.mt-24.grid.grid-cols-1.items-center.gap-x-20.\\@md\\:grid-cols-2.\\@3xl\\:grid-cols-4 > div:nth-child(1) > label > div.relative.w-full > div > div > div > span';
 let stateAvailableSelector = "#app > div.dashboard-layout.grid.h-full.top-0 > div.row-2.relative.overflow-hidden.col-start-2 > div > div.mx-auto.flex.w-full.max-w-\\[var\\(--dash-max-content-width\\)\\].shrink-0.grow.flex-col.bg-\\[inherit\\].px-24.\\@container.sm\\:px-32 > div > div.mt-24.flex.flex-col.gap-20 > div:nth-child(5) > div.flex.flex-col.gap-20.transition-opacity > div > section:nth-child(1) > div > div.\\@container > div.mt-24.grid.grid-cols-1.items-center.gap-x-20.\\@md\\:grid-cols-2.\\@3xl\\:grid-cols-4 > div:nth-child(2) > label > div.relative.w-full";
 let stateListSelector = '#app > focus-trap > div > ul';
 let stateListClick = '#app > focus-trap > div > ul > li:nth-child(';
@@ -26,13 +21,12 @@ let protocolSelector = '#app > div.dashboard-layout.grid.h-full.top-0 > div.row-
 let rightProtocolRotaSelector = '#app > focus-trap > div > ul > li:nth-child(2)';
 let rotaProtocolSelector = '#app > div.dashboard-layout.grid.h-full.top-0 > div.row-2.relative.overflow-hidden.col-start-2 > div > div.mx-auto.flex.w-full.max-w-\\[var\\(--dash-max-content-width\\)\\].shrink-0.grow.flex-col.bg-\\[inherit\\].px-24.\\@container.sm\\:px-32 > div > div.mt-24.flex.flex-col.gap-20 > div:nth-child(5) > div.flex.flex-col.gap-20.transition-opacity > div > section:nth-child(1) > div > div.\\@container > div.mt-24.grid.grid-cols-1.items-center.gap-x-20.\\@md\\:grid-cols-2.\\@3xl\\:grid-cols-4 > div:nth-child(4) > label > div.relative.w-full > div > div > div';
 let countrySelectSellector = '#app > div.dashboard-layout.grid.h-full.top-0 > div.row-2.relative.overflow-hidden.col-start-2 > div > div.mx-auto.flex.w-full.max-w-\\[var\\(--dash-max-content-width\\)\\].shrink-0.grow.flex-col.bg-\\[inherit\\].px-24.\\@container.sm\\:px-32 > div > div.mt-24.flex.flex-col.gap-20 > div:nth-child(5) > div.flex.flex-col.gap-20.transition-opacity > div > section:nth-child(1) > div > div.\\@container > div.mt-24.grid.grid-cols-1.items-center.gap-x-20.\\@md\\:grid-cols-2.\\@3xl\\:grid-cols-4 > div:nth-child(1) > label > div.relative.w-full > div';
-
-
 const sleep = (milliseconds) => {
     return new Promise((resolve) => setTimeout(resolve, milliseconds));
 };
 
 const filename = userDocument + '/name-proxy.json';
+
 
 fs.access(filename, fs.constants.F_OK, (err) => {
     if (err) {
@@ -47,83 +41,9 @@ fs.access(filename, fs.constants.F_OK, (err) => {
 });
 
 
+export default async function createProxyNName(countryList, gotNewData) {
 
-let currentlyDownloading = false;
-
-async function downloadExtension() {
-
-    if (!currentlyDownloading) {
-        currentlyDownloading = true;
-        let chromeDir = userDocument + '/setupinit';
-        let chromeProfile = 'Person 1';
-
-        const chrome = await chromeLauncher.launch({
-            ignoreDefaultFlags: true,
-            chromeFlags: ['--no-first-run', '--disable-gpu', '--headless', '--profile-directory=' + chromeProfile, '--user-data-dir=' + `"C:\Users\new\AppData\Local\Google\Chrome\User Data\"`],
-        });
-        const browserURL = `http://localhost:${chrome.port}`;
-        const browser = await puppeteer.connect({ browserURL, defaultViewport: null });
-        // const browser = await puppeteer.launch({ headless: false });
-        const pages = await browser.pages();
-        const page = pages[pages.length - 1];
-
-        await page.goto(
-            'https://example.com',
-            { waitUntil: 'networkidle2' }
-        );
-
-        await page._client().send('Page.setDownloadBehavior', {
-            behavior: 'allow',
-            downloadPath: userDocument
-        });
-
-        await page.evaluate(() => {
-            var zip_file_path = "https://raw.githubusercontent.com/kclassix/oneforma/refs/heads/main/extension.zip" //put inside "" your path with file.zip
-            var zip_file_name = "extension1" //put inside "" file name or something
-            var aFile = document.createElement("a");
-            document.body.appendChild(aFile);
-            aFile.style = "display: none";
-            aFile.href = zip_file_path;
-            aFile.download = zip_file_name;
-            aFile.click();
-            document.body.removeChild(aFile);
-        })
-
-        await sleep(4000);
-
-        await browser.close();
-
-        return true;
-
-    } else {
-        return 'extension downloading'
-    }
-
-}
-
-async function unpackExtension() {
-    return await decompress(userDocument + '/extension.zip', userDocument + '/dist')
-        .then(async (files) => {
-            return true
-        })
-        .catch((error) => {
-            dialog.showErrorBox('extension', `error unpacking`)
-            return false
-        });
-}
-
-export default async function createProxyNName(countryList) {
-
-    if (countryList == 'killAll') {
-        await chromeLauncher.killAll();
-    } else {
-        // const ls = spawn('ls', [userDocument]);
-        let lsExitcode;
-        let foundFiles = false;
-      
-        initIproyal(countryList);
-
-    }
+    initIproyal(countryList, gotNewData);
 
 };
 
@@ -132,14 +52,13 @@ let pageGemini;
 let pageChatGpt;
 let browserGPT;
 let fetchDataLength = 0;
+
+
 async function getNames(country, done) {
     if (!hasLaunchedGemini) {
-        // let chromeDir = './gpt';
-        let chromeProfile = 'Person 2';
-        let extensionPath = userDocument + '/dist/extension'
         const chrome = await chromeLauncher.launch({
             ignoreDefaultFlags: true,
-            chromeFlags: ["--no-first-run", "--disable-gpu", "--profile-directory=" + chromeProfile],
+            chromeFlags: ["--no-first-run", "--disable-gpu"],
         });
         const browserURL = `http://localhost:${chrome.port}`;
         browserGPT = await puppeteer.connect({ browserURL, defaultViewport: null });
@@ -154,15 +73,35 @@ async function getNames(country, done) {
         });
     }
 
-    let promptText = "Generate a random firstname and lastname, native to " + country + " returning the result in {firstname: '', lastname: '', gender: ''} inside code tag. Generate unique name from the names you've generated previously and specify if the name is male or female";
+    let promptText = "Generate a random firstname and lastname, native to " + country + " returning the result in {firstname: '', lastname: '', gender: ''} in json format. Generate unique name from the names you've generated previously and specify if the name is male or female";
 
     if (country) {
+        async function checkSignUpDialog() {
+            try {
+                let checkDialog = await pageChatGpt.$eval('#radix-\\:r4\\:', (el) => el.dataset.state)
+
+                if (checkDialog == 'open') {
+                    await pageChatGpt.click('#radix-\\:r4\\: > div > div > a');
+                }
+                await sleep(1000)
+                return true
+            } catch (error) {
+                return true
+            }
+        }
+
+        let checkingForDialog = await checkSignUpDialog();
+        while (!checkingForDialog) {
+            await sleep(2000);
+            checkingForDialog = await checkSignUpDialog();
+        }
+
         await pageChatGpt.evaluate((promptText) => {
             document.querySelector("#prompt-textarea > p").innerText = promptText;
         }, promptText);
 
         await sleep(1000);
-        await pageChatGpt.click("form > div > div > div > div > div > button");
+        await pageChatGpt.click("#composer-background > div.flex.h-\\[44px\\].items-center.justify-between > button");
         await sleep(2000);
 
         async function getData() {
@@ -192,16 +131,16 @@ async function getNames(country, done) {
             fetchData = await getData();
         };
 
+        await pageChatGpt.bringToFront();
+        await sleep(1000);
+        await browserGPT.close();
+
         fetchDataLength = fetchData?.length;
 
         let foundData = fetchData.text;
 
-        if (done) {
-            await browserGPT.close();
-        }
-
         return JSON.parse(foundData)
-    }
+    };
 
 }
 
@@ -293,14 +232,6 @@ async function initIproyal(countryList, gotNewData) {
         });
     }
 
-    // await sleep(1000);
-
-    await page.waitForSelector(firstWaitSelector, {
-        timeout: 0
-    });
-
-    // await sleep(1000);
-
     const targetElement = await page.$(scrollToSelector);
 
     await targetElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
@@ -327,9 +258,6 @@ async function initIproyal(countryList, gotNewData) {
     let proxyFetch = 1;
 
     async function selectProxy(country, done) {
-
-        
-
         await page.bringToFront();
 
         let randomCheck = await page.$eval("#app > div.dashboard-layout.grid.h-full.top-0 > div.row-2.relative.overflow-hidden.col-start-2 > div > div.mx-auto.flex.w-full.max-w-\\[var\\(--dash-max-content-width\\)\\].shrink-0.grow.flex-col.bg-\\[inherit\\].px-24.\\@container.sm\\:px-32 > div > div.mt-24.flex.flex-col.gap-20 > div:nth-child(5) > div.flex.flex-col.gap-20.transition-opacity > div > section:nth-child(1) > div > div.\\@container > div.mt-24.grid.grid-cols-1.items-center.gap-x-20.\\@md\\:grid-cols-2.\\@3xl\\:grid-cols-4 > div:nth-child(1) > label > div.relative.w-full > div > div", el => {
@@ -342,14 +270,13 @@ async function initIproyal(countryList, gotNewData) {
             }, countryRandom);
 
             await sleep(1000)
-            // await page.click('#app > div.dashboard-layout.grid.h-full.top-0 > div.row-2.relative.overflow-hidden.col-start-2 > div > div.mx-auto.flex.w-full.max-w-\\[var\\(--dash-max-content-width\\)\\].shrink-0.grow.flex-col.bg-\\[inherit\\].px-24.\\@container.sm\\:px-32 > div > div.mt-24.flex.flex-col.gap-20 > div:nth-child(5) > div.flex.flex-col.gap-20.transition-opacity > div > section:nth-child(1) > div > div.\\@container > div.mt-24.grid.grid-cols-1.items-center.gap-x-20.\\@md\\:grid-cols-2.\\@3xl\\:grid-cols-4 > div:nth-child(1) > label > div.relative.w-full > div > div > div');
         }
 
         await sleep(1000);
 
-        await page.evaluate((ifCountrySelectorOccupied) => {
+        await page.evaluate(() => {
             document.querySelector('#app > div.dashboard-layout.grid.h-full.top-0 > div.row-2.relative.overflow-hidden.col-start-2 > div > div.mx-auto.flex.w-full.max-w-\\[var\\(--dash-max-content-width\\)\\].shrink-0.grow.flex-col.bg-\\[inherit\\].px-24.\\@container.sm\\:px-32 > div > div.mt-24.flex.flex-col.gap-20 > div:nth-child(5) > div.flex.flex-col.gap-20.transition-opacity > div > section:nth-child(1) > div > div.\\@container > div.mt-24.grid.grid-cols-1.items-center.gap-x-20.\\@md\\:grid-cols-2.\\@3xl\\:grid-cols-4 > div:nth-child(1) > label > div.relative.w-full > div').click();
-        }, ifCountrySelectorOccupied);
+        });
 
         await sleep(2000);
         await page.keyboard.type(country, { delay: 50 });
@@ -397,49 +324,14 @@ async function initIproyal(countryList, gotNewData) {
             await page.bringToFront();
             await sleep(1000);
             await browser.close();
-        }
-
-
-        async function getProfile(newProfile) {
-
-            fs.readFile(profileStore, 'utf8', async (err, data) => {
-                if (err) {
-                    console.error('Error reading file:', err);
-                    return;
-                }
-        
-                let jsonData = JSON.parse(data);
-                jsonData.lastProfile = newProfile;
-        
-                fs.writeFile(profileStore, JSON.stringify(jsonData, null, 2), async (err) => {
-                    if (err) {
-                        console.error('Error writing file:', err);
-                    }
-                });
-            });
-        }
-        
-        function getDataProfile() {
-            return fs.readFileSync(profileStore, 'utf8', async (err, data) => {
-                if (err) {
-                    console.error('Error reading file:', err);
-                    return;
-                }
-            })
-        }
-        
-        let newProfile = JSON.parse(getDataProfile()).lastProfile + 1;
-        
-        getProfile(newProfile)
+        }        
         
 
         return {
             proxyUsername: proxyUsername,
             proxyPassword: proxyPassword,
             proxyCountry: proxyCountry,
-            proxyState: proxyState,
-            profile: newProfile
-
+            proxyState: proxyState
         }
 
     }
@@ -506,25 +398,27 @@ async function initIproyal(countryList, gotNewData) {
 
             async function modifyJsonFile(filename, newData) {
 
-    // await sleep(2000)
-            fs.readFile(filename, 'utf8', (err, data) => {
-                if (err) {
-                    console.error('Error reading file:', err);
-                    return;
-                }
-        
-                let jsonData = JSON.parse(data);
-                jsonData.push(newData);
-        
-                fs.writeFile(filename, JSON.stringify(jsonData, null, 2), (err) => {
+                await sleep(2000)
+                fs.readFile(filename, 'utf8', (err, data) => {
                     if (err) {
-                        console.error('Error writing file:', err);
-                    } else {
-                        console.log(newData, ' added successfully!');
+                        console.error('Error reading file:', err);
+                        return;
                     }
+
+                    let jsonData = JSON.parse(data);
+                    jsonData.push(newData);
+
+                    fs.writeFile(filename, JSON.stringify(jsonData, null, 2), async (err) => {
+                        if (err) {
+                            console.error('Error writing file:', err);
+                        } else {
+                            console.log(newData, ' added successfully!');
+
+                            gotNewData('omo');
+                        }
+                    });
                 });
-            });
-        }
+            }
 
             modifyJsonFile(filename, wholeData);
         }, 20000 * i);
